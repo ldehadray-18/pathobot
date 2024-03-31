0be0787f1b8b5b7d6c0856726ffe26c15652b204
lalita <ldehadray@gmail.com>
2024-03-31 13:42:22 +0530

    homeComponent

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import 'bootstrap';
+import { Component, TemplateRef, ViewChild } from '@angular/core';
 import { PatientService } from '../services/patient.service';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 
@@ -10,11 +12,18 @@ import { PatientService } from '../services/patient.service';
 })
 export class HomeComponent {
 
+  @ViewChild('popoverTemplate') popoverTemplate!: TemplateRef<any>;
+  @ViewChild('patientDetailTemplate') patientDetailTemplate!: TemplateRef<any>;
+
+  selectedStatus: boolean = true;
+  selectedStatusMedical: boolean = false;
+  selectedArrival: boolean = false;
   constructor(private PatientData: PatientService) { }
 
   Departments: any = [];
   filteredDepartments: any;
   ngOnInit(): void {
+    console.log(this.selectedStatus);
     this.PatientData.getDepartments().subscribe((data: any[]) => {
       this.Departments = data;
     });
@@ -27,7 +36,26 @@ export class HomeComponent {
       // Check if any ward in the department is checked
       return department.wards.some((ward: any) => { return ward.checked });
     });
+
     console.log("Departments:", this.Departments);
     console.log("Filtered Departments:", this.filteredDepartments);
   }
+  selectedDetail() {
+    console.log(this.selectedStatus);
+    this.selectedStatus = true;
+    this.selectedStatusMedical = false;
+    this.selectedArrival = false;
+
+  }
+  selectedMedical() {
+    this.selectedStatus = false;
+    this.selectedStatusMedical = true;
+    this.selectedArrival = false;
+  }
+  selectedHospitalArrival() {
+    this.selectedStatus = false;
+    this.selectedStatusMedical = false;
+    this.selectedArrival = true;
+  }
+
 }
